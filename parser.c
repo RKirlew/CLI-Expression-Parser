0eeae52da2076b54865e58d6d63086235859939f
R_Kirlew <kirlewraheem@gmail.com>
2024-07-01 20:05:51 -0700

    Update parser.c

--- parser.c
+++ parser.c
@@ -1,26 +1,26 @@
 #include <stdio.h>
 #include <stdlib.h>
+
 #include <stdbool.h>
 
-struct Expression {
-    int numbers[100];
+struct Expression{
+	int numOp;
+	int numLit;
+	int numbers[100];
     char operators[100];
-    int numCount;
-    int opCount;
 };
-
-bool in(char target, const char *str) {
-    for (int i = 0; str[i] != '\0'; i++) {
-        if (str[i] == target) {
+bool in(char toFind, const char* source){
+	for (int i = 0; source[i] != '\0'; i++) {
+        if (source[i] == toFind) {
             return true; 
         }
     }
     return false; 
+	
 }
-
 int evaluateExpression(struct Expression* expr) {
     int result = expr->numbers[0];
-    for (int i = 0; i < expr->opCount; i++) {
+    for (int i = 0; i < expr->numOp; i++) {
         switch (expr->operators[i]) {
             case '+':
                 result += expr->numbers[i + 1];
@@ -34,6 +34,7 @@ int evaluateExpression(struct Expression* expr) {
             case '/':
                 result /= expr->numbers[i + 1];
                 break;
+            
             default:
                 printf("Unsupported operator: %c\n", expr->operators[i]);
                 exit(1);
@@ -42,37 +43,23 @@ int evaluateExpression(struct Expression* expr) {
     return result;
 }
 
-int main(int argc, char *argv[]) {
-    char operators[] = "+-/*";
-    struct Expression expression;
-    expression.numCount = 0;
-    expression.opCount = 0;
+int main(int argc,char *argv[]){
+	struct Expression expression;
+	expression.numOp=0;
+	expression.numLit=0;
+	char operators[] = "+-/*";
 
-    for (int i = 1; i < argc; i++) {  
-        char *arg = argv[i];
-        for (int j = 0; arg[j] != '\0'; j++) {
-            printf("Character: %c\n", arg[j]);
-            if (in(arg[j], operators)) {
-                expression.operators[expression.opCount++] = arg[j];
-            } else {
-                expression.numbers[expression.numCount++] = arg[j] - '0';
-            }
-        }
-    }
-
-    printf("Numbers: ");
-    for (int i = 0; i < expression.numCount; i++) {
-        printf("%d ", expression.numbers[i]);
-    }
-    printf("\nOperators: ");
-    for (int i = 0; i < expression.opCount; i++) {
-        printf("%c ", expression.operators[i]);
-    }
-    printf("\n");
-
-    int result = evaluateExpression(&expression);
+	for(int j=1;j<argc;j++){
+		char *arg=argv[j];
+		for(int t=0;arg[t]!='\0';t++){
+			printf("Characters to eval:%c\n",arg[t]);
+			if(in(arg[t],operators)){
+				expression.operators[expression.numOp++]=arg[t];
+			}else{
+				expression.	numbers[expression.numLit++]=arg[t]-'0';
+			}
+		}
+	}
+	int result = evaluateExpression(&expression);
     printf("Result of the expression: %d\n", result);
-
-    return 0;
 }
-
